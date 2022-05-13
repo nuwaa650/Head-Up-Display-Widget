6f06aae33a6802608d1184040eb94f03a8240843
Nadeesha Nuwanthika <nadeeshanuwanthika99@gmail.com>
2022-05-14 04:59:49 +0530

    Update code

--- speedometer/app/src/main/java/com/example/speedometer/MainActivity.kt
+++ speedometer/app/src/main/java/com/example/speedometer/MainActivity.kt
@@ -1,20 +1,17 @@
+import com.google.android.gms.location.*
+import java.util.jar.Manifest
 
 +package com.example.speedometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.location.ActivityTransitionRequest
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
 import com.google.android.material.chip.ChipDrawable
 import pub.devrel.easypermissions.EasyPermissions
 import java.util.*
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity(),EasyPermissions.PermissionCallbacks,
-EasyPermissions.RationaleCallbacks
-{
+EasyPermissions.RationaleCallbacks {
     private val TAG = "MainActivity"
     private val LOCATION_PERM = 124
     private val speedUpStartTime = 0L
@@ -25,19 +22,46 @@ EasyPermissions.RationaleCallbacks
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
-    private var isDone :Boolean by Delegates.observable(initialValue:false){property,oldValue,newValue->
-        if (newValue == true)
-        {
-            fusedLocationProviderClient
+    private var isDone: Boolean by Delegates.observable(initialValue:false)
+    {
+        property, oldValue, newValue->
+        if (newValue == true) {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
         }
     }
-
-//new one
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        askForLocationPermission()
+        createLocationRequest()
+
+
+    }
+
+     fun askForLocationPermission(){
+        if (hasLocationpermissions){
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListner{location:Location? ->
+
+            }
+            else
+            {
+                EasyPermissions.requestPermissions(
+                        host:this
+                        rationale:"need permissions to find your location and calculate the speed",
+                        LOCATION_PERM,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+
+        }
+    }
+
+    private fun hasLocationPermissions():Boolean{
+        return EasyPermissions.hasPermissions(context:this,android.Mainfest.permission.ACCESS_FINE_LOCATION)
     }
 }
